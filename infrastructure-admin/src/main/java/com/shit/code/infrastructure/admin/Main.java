f4268cec09b07899d6210f58f5a47a0794e31926
anthonychen <anthonychen@lexin.com>
2020-07-03 17:01:31 +0800

    admin

--- infrastructure-admin/src/main/java/com/shit/code/infrastructure/admin/Main.java
+++ infrastructure-admin/src/main/java/com/shit/code/infrastructure/admin/Main.java
@@ -1,11 +1,20 @@
 package com.shit.code.infrastructure.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Anthony Chen
  * @date 2020/7/3
  **/
 @Slf4j
+@SpringBootApplication
+//@EnableDiscoveryClient
+@EnableAdminServer
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
