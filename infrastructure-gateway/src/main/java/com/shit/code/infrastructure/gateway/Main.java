9a20a9ecbb9f53373cb4f44b1d60c7ad0cb17c9c
anthonychen <anthonychen@lexin.com>
2020-07-03 14:17:11 +0800

    gateway

--- infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/Main.java
+++ infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/Main.java
@@ -1,11 +1,19 @@
 package com.shit.code.infrastructure.gateway;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Anthony Chen
  * @date 2020/7/3
  **/
 @Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
