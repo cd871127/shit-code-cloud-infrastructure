a38cb3282e91a8e563c20a97acfae98c22510293
anthonychen <anthonychen@lexin.com>
2020-07-06 19:26:59 +0800

    jar name

---
+++ infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/route/PersistenceRouteDefinitionRepository.java
@@ -0,0 +1,60 @@
+package com.shit.code.infrastructure.gateway.route;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.Collections.synchronizedMap;
+
+/**
+ * @author Anthony Chen
+ * @date 2020/7/6
+ **/
+@Slf4j
+@Component
+public class PersistenceRouteDefinitionRepository implements RouteDefinitionRepository {
+    private final Map<String, RouteDefinition> routes = synchronizedMap(
+            new LinkedHashMap<String, RouteDefinition>());
+
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        log.info("查询2");
+        return Flux.fromIterable(routes.values());
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        log.info("保存2");
+        return route.flatMap(r -> {
+            if (StringUtils.isEmpty(r.getId())) {
+                return Mono.error(new IllegalArgumentException("id may not be empty"));
+            }
+            routes.put(r.getId(), r);
+            //发mq
+            return Mono.empty();
+        });
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        log.info("删除2");
+        return routeId.flatMap(id -> {
+            if (routes.containsKey(id)) {
+                routes.remove(id);
+                return Mono.empty();
+            }
+            //发mq
+            return Mono.defer(() -> Mono.error(
+                    new NotFoundException("RouteDefinition not found: " + routeId)));
+        });
+    }
+}
