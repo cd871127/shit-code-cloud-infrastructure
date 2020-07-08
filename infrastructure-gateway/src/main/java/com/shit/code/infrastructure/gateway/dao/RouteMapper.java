e7e8556bead8d054b359a3f0e4985f9ccd928cb4
anthonychen <anthonychen@lexin.com>
2020-07-08 22:17:53 +0800

    bootjar

--- infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/dao/RouteMapper.java
+++ infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/dao/RouteMapper.java
@@ -1,12 +1,12 @@
 package com.shit.code.infrastructure.gateway.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Anthony Chen
  * @date 2020/7/7
  **/
-@Mapper
+//@Mapper
 public interface RouteMapper {
 
 
---
+++ infrastructure-gateway/src/main/resources/db/migration/V1.0__route.sql
@@ -0,0 +1,4 @@
+create table t_route
+(
+    id int comment 'route_id' primary key
+);
