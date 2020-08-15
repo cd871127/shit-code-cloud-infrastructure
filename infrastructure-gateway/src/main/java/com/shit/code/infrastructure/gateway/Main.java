5f9423f96d740a60c575c8b9300e0cf910d1c650
Anthony <cdistc@163.com>
2020-08-15 14:03:24 +0800

    redis

--- infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/Main.java
+++ infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/Main.java
@@ -1,13 +1,9 @@
 package com.shit.code.infrastructure.gateway;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Anthony Chen
@@ -16,20 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
-@RequestMapping("/test")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Value("${test1}")
-    private String test1;
-    @Value("${test2}")
-    private String test2;
-
-    @GetMapping("test")
-    String test() {
-        return test1 + test2;
     }
 }
--- infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/dao/RouteMapper.java
+++ infrastructure-gateway/src/main/java/com/shit/code/infrastructure/gateway/dao/RouteMapper.java
@@ -10,5 +10,4 @@ package com.shit.code.infrastructure.gateway.dao;
 public interface RouteMapper {
 
 
-
 }
