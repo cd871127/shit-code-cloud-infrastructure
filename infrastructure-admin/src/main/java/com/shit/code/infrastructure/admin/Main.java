cbdbf55239932eb2d7c7b79b1abb53dc633359ca
Anthony <cdistc@163.com>
2020-08-15 13:27:17 +0800

    config

--- infrastructure-admin/src/main/java/com/shit/code/infrastructure/admin/Main.java
+++ infrastructure-admin/src/main/java/com/shit/code/infrastructure/admin/Main.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Anthony Chen
