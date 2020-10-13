613f200027e7bded1f57cf7432a72cdb7cf34593
anthonychen <anthonychen@lexin.com>
2020-10-13 18:38:29 +0800

    fix

---
+++ infrastructure-gateway/src/main/java/com/shit/code/cloud/infrastructure/gateway/configuration/GateWayConfiguration.java
@@ -0,0 +1,21 @@
+package com.shit.code.cloud.infrastructure.gateway.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+/**
+ * @author anthonychen
+ * @date 2020/10/13
+ **/
+@Slf4j
+@Configuration
+public class GateWayConfiguration {
+
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
+
+}
