64e94d8b401ca037fe3a062d44d7e9e20d2c44dd
anthonychen <anthonychen@lexin.com>
2020-10-14 17:06:45 +0800

    security

---
+++ infrastructure-security/src/main/java/com/shit/code/cloud/infrastructure/security/Main.java
@@ -0,0 +1,18 @@
+package com.shit.code.cloud.infrastructure.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author anthonychen
+ * @date 2020/10/14
+ **/
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
