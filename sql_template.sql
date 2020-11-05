23df263c02181a02f877a47ace18fea3291cddc2
Anthony <cdistc@163.com>
2020-11-06 00:50:01 +0800

    format code

--- sql_template.sql
+++ sql_template.sql
@@ -1,11 +1,11 @@
 create table IF NOT EXISTS ${dn_name}_db.t_${table_name}
 (
-    id          int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
-    version     INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '乐观锁版本',
-    data_status varchar(12)  NOT NULL default 'INVALID' COMMENT '数据状态',
-    deleted     varchar(3)   NOT NULL default 'NO' COMMENT '是否已经删除',
-    created_by   VARCHAR(64)  NOT NULL default 'SYSTEM' COMMENT '创建人',
-    created_date DATETIME     NOT NULL default now() COMMENT '创建时间',
+    id                 int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+    version            INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '乐观锁版本',
+    data_status        varchar(12)  NOT NULL default 'INVALID' COMMENT '数据状态',
+    deleted            varchar(3)   NOT NULL default 'NO' COMMENT '是否已经删除',
+    created_by         VARCHAR(64)  NOT NULL default 'SYSTEM' COMMENT '创建人',
+    created_date       DATETIME     NOT NULL default now() COMMENT '创建时间',
     last_modified_by   VARCHAR(64)  NOT NULL default 'SYSTEM' COMMENT '修改人',
     last_modified_date DATETIME     NOT NULL default now() COMMENT '修改时间',
     PRIMARY KEY (id),
